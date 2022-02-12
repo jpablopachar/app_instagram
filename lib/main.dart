a1f5dcc520a1a3ec9164168c294a840fe6af19b4
pablo.pachar <pablo.pachar@exmerdev.com>
2022-02-11 20:21:47 -0500

    Integracion de firebase en web y movil

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,23 @@ import 'package:app_instagram/responsive/responsive_layout_screen.dart';
 import 'package:app_instagram/responsive/web_layout_screen.dart';
 import 'package:app_instagram/utils/colors.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: 'AIzaSyDRzKqgi02yA0O89_uazeBBymOhdW7X5r4',
-          appId: '1:336827675620:web:8dbd8cbfa67f5d01834df1',
-          messagingSenderId: '336827675620',
-          projectId: 'app-instagram-b4942',
-          storageBucket: 'app-instagram-b4942.appspot.com'));
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: 'AIzaSyDRzKqgi02yA0O89_uazeBBymOhdW7X5r4',
+            appId: '1:336827675620:web:8dbd8cbfa67f5d01834df1',
+            messagingSenderId: '336827675620',
+            projectId: 'app-instagram-b4942',
+            storageBucket: 'app-instagram-b4942.appspot.com'));
+  } else {
+    await Firebase.initializeApp();
+  }
 
   runApp(const MyApp());
 }
